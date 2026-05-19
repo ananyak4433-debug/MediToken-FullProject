ff3a7b4ce124828b039565eaeaf7964c9b7997fe
Ananya K <ananyak4433@gmail.com>
2026-05-19 21:27:26 +0530

    Fixed MongoDB

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -17,12 +17,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+
+    console.log("DB URI =", process.env.DB_URI);
+
     const conn = await mongoose.connect(process.env.DB_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected:", conn.connection.host);
+
   } catch (err) {
     console.log("Database Connection Failed:", err.message);
-    process.exit(1);
   }
 };
 
