0a1b062f3c0ec8c6cab05968a311fdff7f00038b
Ananya K <ananyak4433@gmail.com>
2026-05-19 21:24:08 +0530

    Fixed MongoDB connection

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,12 +1,29 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost:27017/MEDITOKEN");
+//         console.log("Database Connected Successfully");
+//     } catch (err) {
+//         console.log("Database Connection Failed:", err.message);
+//     }
+// };
+
+// module.exports = connectDB;
+
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/MEDITOKEN");
-        console.log("Database Connected Successfully");
-    } catch (err) {
-        console.log("Database Connection Failed:", err.message);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log("Database Connection Failed:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
